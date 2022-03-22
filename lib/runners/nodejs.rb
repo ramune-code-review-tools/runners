438a9a06b9ef8b82a198cdcd1eac8c9d0c258e64
Radomír Bosák <radomir.bosak@gmail.com>
2022-03-22 18:34:20 +0900

    Revert "Avoid cache during npm install (#2988)" (#2989)
    This reverts commit 6790a357e01d64aae27479247ba0429eab5f4b99.

--- lib/runners/nodejs.rb
+++ lib/runners/nodejs.rb
@@ -147,7 +147,6 @@ module Runners
         --no-progress
         --no-save
         --no-update-notifier
-        --prefer-online
       ] + flags
 
       trace_writer.message "Installing npm dependencies..."
